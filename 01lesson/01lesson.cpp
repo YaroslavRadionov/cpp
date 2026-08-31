f22152a59de590b7444a8ad795831864a7805687
radio <radio@JARIK_PC>
2026-08-31 20:50:00 +0300

    Add project files.

---
+++ 01lesson/01lesson.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float dymes = 45;
+	float cm = dymes * 2.54;
+	cout << cm<<endl;
+
+
+	float eat = 20;
+	float buy = eat * 30 / 1000;
+	cout << buy<<endl;
+
+
+
+
+
+return 0;}
+
+
---
+++ 1_lesson/1lesson.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "I\n\tLove\n\t\tyou\n\t\t\tC++!\n\n\n";
+	cout << "\t...:::RESUME:::...\n\n\nName:\t\tYarik\nSurname:\tRadionov\nLast name:\tVolodumurovuch\n\nDate of births:\t\t02.05.2012\nVillage:Veluka Omelyana\nAge:\t\t14\n\n\nHobby:\tC++\n:.........................:";
+
+
+
+
+
+
+	return 0;}
